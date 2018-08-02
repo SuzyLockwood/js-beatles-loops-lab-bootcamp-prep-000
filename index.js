8a3dde0d91be2a6657c8fcf8ad00ad79c92089b5
Susan Lockwood <susan.m.mullin@gmail.com>
2018-08-02 05:22:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 function theBeatlesPlay(players, instruments) {
     var allPlayers = [];
     for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
+        allPlayers.push(players[i] + ' plays ' + instruments[i]);
     }
     return allPlayers;
 }
  function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        newFacts.push(facts[i] + '!!!');
         i++;
     }
     return newFacts;
@@ -19,7 +19,7 @@ function theBeatlesPlay(players, instruments) {
  function iLoveTheBeatles(num) {
     var love = [];
     do {
-        love.push("I love the Beatles!");
+        love.push('I love the Beatles!');
         num++;
     } while (num < 15);
     
