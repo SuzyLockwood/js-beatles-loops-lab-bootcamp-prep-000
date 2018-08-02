410292ae8ed10c5dc96480534824854879a6eda4
Susan Lockwood <susan.m.mullin@gmail.com>
2018-08-02 05:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function theBeatlesPlay(players, instruments) {
         "He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
-        "He hated the sound of his own voice",
-        "foo",
-        "bar"
+        "He hated the sound of his own voice"
       ];
     var newFacts = [];
     var i = 0;
